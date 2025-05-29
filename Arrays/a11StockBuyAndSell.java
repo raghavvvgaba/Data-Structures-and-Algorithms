e0c5e27cf8af5906e59b4930a905a104afb06c4a
Raghav <gabasraghav2016@gmail.com>
2025-05-29 23:34:22 +0530

    11

---
+++ Arrays/a11StockBuyAndSell.java
@@ -0,0 +1,33 @@
+public class a11StockBuyAndSell {
+    // Naive
+    // static int maxProfit(int price[], int start, int end) {
+    // if (end <= start)
+    // return 0;
+    // int profit = 0;
+    // for (int i = start; i < end; i++) {
+    // for (int j = i+1; j <= end; j++) {
+    // if(price[i]>price[j]){
+    // int curr_profit = price[j] - price[i] + maxProfit(price, start, i-1) +
+    // maxProfit(price, j+1, end);
+    // profit = Math.max(curr_profit, profit);
+    // }
+    // }
+    // }
+    // return profit;
+    // }
+    static int maxProfit(int price[]) {
+        int n = price.length;
+        int profit = 0;
+        for (int i = 1; i < n; i++) {
+            if (price[i] > price[i - 1]) {
+                profit += (price[i] - price[i - 1]);
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 5, 3, 8, 12 };
+        System.out.println(maxProfit(arr));
+    }
+}
