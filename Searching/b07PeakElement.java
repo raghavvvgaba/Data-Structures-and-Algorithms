4f4d4d9af7dd62610f7f337a81ebb96adcb110c0
Raghav <gabasraghav2016@gmail.com>
2025-07-03 22:52:55 +0530

    Search in sorted rotated array; Search peak element

---
+++ Searching/b07PeakElement.java
@@ -0,0 +1,34 @@
+package Searching;
+
+public class b07PeakElement {
+    static int peakNaive(int arr[]) {
+        int n = arr.length;
+        if (n == 1)
+            return arr[0];
+        if (arr[0] >= arr[1])
+            return arr[0];
+        if (arr[n - 1] >= arr[n - 2])
+            return arr[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1])
+                return arr[i];
+        }
+        return -1;
+    }
+
+    static int peak(int arr[]) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (mid == 0 || arr[mid] > arr[mid - 1] && mid == n - 1 || arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > 0 && arr[mid - 1] >= arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+}
